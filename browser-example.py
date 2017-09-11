f9019eb74c18198f9e2d3096cf4b379780ab636a
pesutsinga <31654326+pesutsinga@users.noreply.github.com>
2017-09-11 05:32:04 -0400

    Update browser-example.py

--- browser-example.py
+++ browser-example.py
@@ -5,11 +5,11 @@ USERNAME = ''
 PASSWORD = ''
 
 #either 'Facility' or 'PC'
-BOOKING_MODE = "PC"
+BOOKING_MODE = 'PC'
 from splinter import Browser
 chrome = Browser('chrome')
 def timeDelay(time):
-    chrome.is_element_present_by_name('!@#$%^^%$#@!', wait_time=time)
+    chrome.is_element_present_by_name('!@#$%^&*())(*&^%$#@!', wait_time=time)
 
 def login_ntupcb():
 
@@ -23,8 +23,7 @@ def login_ntupcb():
     # Interact with elements
     button.click()
 
-def main():
-    login_ntupcb()
+def scrape_pc():
     if BOOKING_MODE == 'PC':
         button = chrome.find_by_id('tdPcBook')
         button.click()
@@ -32,10 +31,19 @@ def main():
             iframe.find_by_id('pnlInsLoc3').click()
             iframe.find_by_id('pnlInsPcGrp0').click()
         for value in [31, 26, 7, 27]:
-            chrome.select('drplistpcgrp', value)
-            timeDelay(0.1);
+            url = 'https://ntupcb.ntu.edu.sg/fbscbs/PCBooking/SeatingParentForm.aspx?LocId=2&PcGrpId=' + str(value)
+            chrome.visit(url)
             with chrome.get_iframe('frmSeating') as iframe:
-                print(iframe.html)
+                tables = iframe.find_by_css('.ItemStyleProp')
+                for table in hooray:
+                    if table.find_by_tag('img'):
+                        if table.text != '':
+                            print(">>>")
+                            print(table.text)
+
+def main():
+    login_ntupcb()
+    scrape_pc()
 
 if __name__ == '__main__':
     main()
