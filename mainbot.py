52013097f61608206272083675c31bb858dd00b5
Jerrell Ezralemuel <pesutsinga@gmail.com>
2017-09-24 00:29:14 +0700

    flake8 approved

--- mainbot.py
+++ mainbot.py
@@ -5,13 +5,18 @@ import chopeDB
 import datetime
 import logging
 
+
 def assert_error(bot, update):
     chat_id = update.message.chat_id
     text = "Last bot restart: " + curTime + "Please redo /start"
     bot.send_message(chat_id=chat_id, text=text)
 
+
 def assert_no_username(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="mirna pls.\nset username in settings")
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="mirna pls.\nset username in settings")
+
 
 def start(bot, update, args):
     chat_id = update.message.chat_id
@@ -24,17 +29,21 @@ def start(bot, update, args):
     if not chopeDB.correct_credential(update.message.chat_id):
         print
 
+
 def initHandler():
     cmd_start = CommandHandler('start', start, pass_args=True)
     dispatcher.add_handler(cmd_start)
-    
+
+
 def main():
     startTime = strftime("%d %b %Y %H:%M:%S", gmtime())
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
     updater = Updater(token='377140861:AAEiMIj-VOwB68HcftvMILjr5wc6LJJml6g')
     dispatcher = updater.dispatcher
-    
+
     updater.start_polling()
 
 if __name__ == "__main__":
