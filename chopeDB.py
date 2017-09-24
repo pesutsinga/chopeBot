b1e8de074ba83209eb93352d47836d3da384fea5
Jerrell Ezralemuel <pesutsinga@gmail.com>
2017-09-24 22:44:08 +0700

    barebones chopeDB
    Signed-off-by: Jerrell Ezralemuel <pesutsinga@gmail.com>

--- chopeDB.py
+++ chopeDB.py
@@ -1,2 +1,30 @@
-def correct_credential(chat_id):
-    return True
+import pymysql
+
+
+class MySQLConnector:
+    table = ''
+
+    def __init__(self):
+        self.table = ''
+        self.db = pymysql.connect(
+            host='bebong.id',
+            user='u7728567_chope',
+            password='bunuakumz578%&*',
+            db='u7728567_chopebot',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+
+    def set_table(tablename):
+        self.table = str(tablename)
+
+    def update(**conditions, **values):
+        pass
+
+    def delete(**condition):
+        pass
+
+    def insert(**values):
+        pass
+
+    def select(**columns, **conditions, ext=''):
+        pass
