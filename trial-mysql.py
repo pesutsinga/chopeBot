afddc9e2267f9a15d682b7db833b762b77eba39b
Ubuntu <apasi@ubuntu1704.vl3dwvzwfw2evdt3ml4bsgw0aa.ix.internal.cloudapp.net>
2017-09-27 09:37:26 +0000

    gabeda sih

--- trial-mysql.py
+++ trial-mysql.py
@@ -2,14 +2,14 @@ from chopeDB import MySQLConnector
 
 
 def main():
-    db = MySQLConnector('LIBCHOP')
-    args = []
-    args.append(("TELEGRAMID", 's', 'lasidf.2830'))
-    args.append(("USERNAME", 's', 'HOBO'))
-    args.append(("PASSWORD", 's', 'mancay'))
-    db.insert("LIBCHOP", args)
+    db = MySQLConnector('u7728567_chopebot')
+    argum = []
+    argum.append(("TELEGRAMID", 's', 'apalahakuini' ))
+    argum.append(("USERNAME", 's', 'gabisaapaapa'))
+    argum.append(("PASSWORD", 's', 'mengalakepadatuhan'))
+    db.insert("LIBCHOP", argum)
     db.close()
-
+    
 
 if __name__ == '__main__':
     main()
